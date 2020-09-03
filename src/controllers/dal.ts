c8740d7ff4bc0945606474f5a1b2db41fae068eb
idoraz <idoraz84@gmail.com>
2020-09-03 22:17:05 +0300

    Removed redundant lines

--- src/controllers/dal.ts
+++ src/controllers/dal.ts
@@ -3,9 +3,7 @@ import mongoose from 'mongoose';
 import { MONGODB_URI } from '../util/secrets';
 
 export default async function (): Promise<Db> {
-    console.log(MONGODB_URI);
     const uri: string = MONGODB_URI || 'mongodb://127.0.0.1:27017/momskitchen';
-    console.log(uri);
     const db = mongoose.connect(uri, {
         useMongoClient: true
     });
