2077495ddc82270f0cc470aa916aae0a387f04a5
idoraz <idoraz84@gmail.com>
2020-09-04 02:53:27 +0300

    Update ecosystem

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,17 +1,23 @@
 module.exports = {
     apps: [
         {
-            name: "momsKitchen",
-            script: "dist/server.js",
+            name: 'momsKitchen',
+            script: 'dist/server.js',
             watch: false,
             env: {
-                "NODE_ENV": "development",
+                NODE_ENV: 'development',
+                MONGODB_URI:
+                    'mongodb://admin2:Ido250484@momskitchen-shard-00-00.vlaea.mongodb.net:27017,momskitchen-shard-00-01.vlaea.mongodb.net:27017,momskitchen-shard-00-02.vlaea.mongodb.net:27017/momskitchen?ssl=true&replicaSet=atlas-11wwuc-shard-0&authSource=admin&retryWrites=true&w=majority',
+                SESSION_SECRET: 'ashdfjhasdlkjfhalksdjhflak'
             },
             env_production: {
-                "NODE_ENV": "production"
+                NODE_ENV: 'production',
+                MONGODB_URI:
+                    'mongodb://admin2:Ido250484@momskitchen-shard-00-00.vlaea.mongodb.net:27017,momskitchen-shard-00-01.vlaea.mongodb.net:27017,momskitchen-shard-00-02.vlaea.mongodb.net:27017/momskitchen?ssl=true&replicaSet=atlas-11wwuc-shard-0&authSource=admin&retryWrites=true&w=majority',
+                SESSION_SECRET: 'ashdfjhasdlkjfhalksdjhflak'
             },
             instances: 2,
-            exec_mode: "cluster",
-        },
+            exec_mode: 'cluster'
+        }
     ]
-}
+};
